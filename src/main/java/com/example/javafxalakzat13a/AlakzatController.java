c47d37f649c7a9854ec82d807c932d9f8ba9f077
Zsolt Harmath <harmathzs@gmail.com>
2025-09-17 13:36:07 +0200

    Show listview data

--- src/main/java/com/example/javafxalakzat13a/AlakzatController.java
+++ src/main/java/com/example/javafxalakzat13a/AlakzatController.java
@@ -1,9 +1,11 @@
 package com.example.javafxalakzat13a;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -18,6 +20,12 @@ public class AlakzatController {
     @FXML public ListView<String> listview_Listview;
     @FXML public ImageView imageview_Alakzat;
     @FXML public Pane pane_Alakzat;
+    @FXML public RadioButton radioPiros;
+    @FXML public RadioButton radioZold;
+    @FXML public RadioButton radioKek;
+    @FXML public RadioButton radioNegyzet;
+    @FXML public RadioButton radioKor;
+    @FXML public RadioButton radioHaromszog;
     @FXML
     private Label welcomeText;
 
@@ -50,5 +58,33 @@ public class AlakzatController {
 
     public void onHaromszogSelected(ActionEvent actionEvent) throws FileNotFoundException {
         imageview_Alakzat.setImage(new Image("file:icons/haromszog.png"));
+    }
+
+    public void onHozzaadClick(ActionEvent actionEvent) {
+        ObservableList<String> listviewLines = listview_Listview.getItems();
+
+        String newline = "";
+
+        if (radioPiros.isSelected()) {
+            newline += "Piros, ";
+        }
+        if (radioZold.isSelected()) {
+            newline += "Zöld, ";
+        }
+        if (radioKek.isSelected()) {
+            newline += "Kék, ";
+        }
+
+        if (radioKor.isSelected()) newline+= "Kör";
+        if (radioHaromszog.isSelected()) newline+= "Háromszög";
+        if (radioNegyzet.isSelected()) newline+= "Négyzet";
+
+        //listviewLines.add("Piros, Kör");
+
+        // TODO
+        if (!newline.isEmpty()) listviewLines.add(newline);
+
+        listview_Listview.setItems(listviewLines);
+        listview_Listview.getSelectionModel().selectLast();
     }
 }
